bb128fe6eccf3d00ee9fc6fcf9c03e838ec8562c
Luccas Aguilar <luccasaguilar@gmail.com>
2022-03-22 21:42:00 -0400

    Alteracao do usuario

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,6 +12,7 @@
         @foreach ($users as $user)
             <li>
                 {{$user->name}} - {{$user->email}} | <a href="{{ route('users.show', $user->id) }}" > Detalhes </a>
+                | <a href="{{ route('users.edit', $user->id) }}" > Editar </a>
             </li>
 
         @endforeach
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('users', [UserController::class, 'store'])->name('users.store');
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 
 Route::get('/', function () {
